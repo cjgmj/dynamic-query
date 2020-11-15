0450f19ac2f1bce997fa1d15cd0e83ce0b358db5
cjgmj <cjgmj@hotmail.com>
2020-11-15 17:21:59 +0100

    Application base

---
+++ src/test/java/com/cjgmj/dynamicQuery/DynamicQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjgmj.dynamicQuery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
